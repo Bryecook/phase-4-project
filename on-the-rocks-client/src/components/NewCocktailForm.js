76b1d5e2129712fef40888a0708bcf7f0be5cb30
Jose Mendoza <pball626@gmail.com>
2021-05-14 11:15:41 -0500

    submit drink

--- on-the-rocks-client/src/components/NewCocktailForm.js
+++ on-the-rocks-client/src/components/NewCocktailForm.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react'
 
 export default class NewCocktailForm extends Component {
-    render(){
+
+    submitHandler = (e) => {
+        e.preventDefault()
+        console.log('formsubmitted', this.props)
+        this.props.createCocktail(this.state)
+
+    }
+
+
+    changeHandler = (e) => {
+        let { name, value } = e.target
+        this.setState({
+            [name]: value
+        })
+    }
+
+
+    render() {
         return (
             <div>
                 <h1>newcocktail form</h1>
                 <div>
                     <form onSubmit={this.submitHandler}>
-                        <input type='text' name='name' placeholder='Name of Drink'></input><br/><br/>
-                        <textarea type='text' name='name' placeholder='Ingredients needed' rows={10} ></textarea><br/><br/>
-                        <textarea type='text' name='name' placeholder='Instructions'></textarea><br/><br/>
-                        <input type='text' name='name' placeholder='Type of Glass'></input><br/><br/>
-                        <input type='text' name='picture' placeholder='Image URL'></input><br/><br/>
-                        <input type="submit" name="submit" value="Create New Cocktail" className="submit"/>
+                        <input type='text' name='name' placeholder='Name of Drink' onChange={this.changeHandler} ></input><br /><br />
+                        <textarea type='text' name='name' placeholder='Ingredients needed' rows={10}onChange={this.changeHandler}  ></textarea><br /><br />
+                        <textarea type='text' name='name' placeholder='Instructions' onChange={this.changeHandler} ></textarea><br /><br />
+                        <input type='text' name='name' placeholder='Type of Glass' onChange={this.changeHandler} ></input><br /><br />
+                        <input type='text' name='picture' placeholder='Image URL' onChange={this.changeHandler} ></input><br /><br />
+                        <input type="submit" name="submit" value="Create New Cocktail" className="submit" />
                     </form>
                 </div>
             </div>
