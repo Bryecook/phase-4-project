c90c8e227aca8662d6ba8271a4933a54c4a66e93
Jose Mendoza <pball626@gmail.com>
2021-05-14 10:45:47 -0500

    cocktailform

--- on-the-rocks-client/src/components/NewCocktailForm.js
+++ on-the-rocks-client/src/components/NewCocktailForm.js
@@ -5,6 +5,16 @@ export default class NewCocktailForm extends Component {
         return (
             <div>
                 <h1>newcocktail form</h1>
+                <div>
+                    <form onSubmit={this.submitHandler}>
+                        <input type='text' name='name' placeholder='Name of Drink'></input><br/><br/>
+                        <textarea type='text' name='name' placeholder='Ingredients needed' rows={10} ></textarea><br/><br/>
+                        <textarea type='text' name='name' placeholder='Instructions'></textarea><br/><br/>
+                        <input type='text' name='name' placeholder='Type of Glass'></input><br/><br/>
+                        <input type='text' name='picture' placeholder='Image URL'></input><br/><br/>
+                        <input type="submit" name="submit" value="Create New Cocktail" className="submit"/>
+                    </form>
+                </div>
             </div>
         )
     }
