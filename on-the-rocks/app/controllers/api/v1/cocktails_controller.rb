cbffd9cfe1af6410e295a821b34e1d12b9a2a414
Jose Mendoza <pball626@gmail.com>
2021-05-14 10:34:46 -0500

    finishing-login-rerender

--- on-the-rocks/app/controllers/api/v1/cocktails_controller.rb
+++ on-the-rocks/app/controllers/api/v1/cocktails_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::CocktailsController < ApplicationController
     before_action :logged_in?
     def index
         cocktails = Cocktail.all
-        render json: cocktails
+        render json:a cocktails
     end
     
 end
